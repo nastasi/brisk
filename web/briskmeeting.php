8982b48a3160f8d83e77722be04466ee9a5fae3d
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2008-08-01 07:46:50 +0000

    brisk meeting added

---
+++ web/briskmeeting.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+<title>Brisk Meeting</title>
+<link rel="stylesheet" type="text/css" href="briskmeeting.css">
+<!-- <link rel="stylesheet" type="text/css" href="room.css">
+<link rel="stylesheet" type="text/css" href="bug.css"> -->
+<script type="text/javascript" src="commons.js"></script>
+</head>
+<body style="font-family: verdana, sans-serif; font-size: 14px;">
+<div style="margin: 40px auto auto auto; text-align: center;">
+<div style=" z-index: 0; margin: auto; background-color: #fafafa; background-repeat: no-repeat; background-image: URL('img/brisk_half.png'); background-position: center right; text-align: left; border: 1px solid black; position: relative; left: auto; width: 756px;">
+<img style="z-index: 50; position: absolute; border: 0px solid black; left: -26px; top: -26px; margin: 0px;" src="img/brisk_meeting_title_hi.png">
+<img style="z-index: 50; position: absolute; border: 0px solid black; left: -26px; top: 59px; margin: 0px;" src="img/brisk_meeting_title_low.png">
+<div style=" z-index: 0; margin: auto; text-align: left; padding: 70px 10px 10px 80px; border: 0px solid black; position: relative; left: auto;">
+<h4>Presentazioni</h4>
+Ciao a tutti,<br>
+siamo due assidui frequentatori del sito, Bego e Manuel(K2).<br>
+<br>
+Abbiamo organizzato due meeting per poterci incontrare fra briskisti,
+conoscerci meglio e soprattutto passare una giornata di divertimento
+giocando a briscola chiamata.<br>
+<h4>Date dei meeting</h4>
+<ul>
+<li>30 agosto - <a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=sarnico,+italy&sll=37.0625,-95.677068&sspn=52.902929,57.65625&ie=UTF8&z=13">Sarnico (BG)</a> sul lago d'Iseo</li>
+<li>7 settembre - vicinanze di <a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=parma,+italy&ie=UTF8&ll=44.80425,10.332642&spn=0.74832,0.900879&z=10&iwloc=addr" style="color:#0000FF;text-align:left">Parma (PR)</a></li>
+</ul>
+<h4>Come si articoleranno le giornate</h4>
+<ul>
+<li>ritrovo in tarda mattinata (9:30-10:00)</li>
+
+<li>saluti e convenevoli</li>
+
+<li>pranzo non  troppo impegnativo</li>
+
+<li>prima e dopo pranzo <b>tanta e tanta briscola</b>.</li>
+</ul>
+<br>
+Il termine serale &egrave; lasciato naturalmente alle esigenze di ognuno.<br>
+
+<h4>Contatti</h4>
+Chi fosse interessato a partecipare a uno o entrambi questi eventi, oppure
+volesse delucidazioni, non esiti a contattare: <br>
+<dl>
+<dd><a href="mailto:grandebego@hotmail.it">Bego</a></dd>
+<dd><a href="mailto:MANUEL-K2@hotmail.it">Manuel-K2</a></dd>
+</dl>
+
+<h4>Aggiornamenti</h4>
+Fra pochi giorni renderemo noti, a chi vorr&agrave; partecipare, i dettagli  e le
+indicazioni stradali per raggiungere i siti dei 2 incontri.<br>
+<br>
+Saluti a tutti, Bego e Manuel.<br>
+<br>
+</div>
+</div>
+</div>
+<!-- </div> -->
+</body>
+</html>
