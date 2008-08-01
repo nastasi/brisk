8e498993c78dd983268c76fcad41afaf445fc699
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2008-08-01 07:58:09 +0000

    prime versioni per i file del BUG

---
+++ web/bug.php
@@ -0,0 +1,73 @@
+<html>
+<head>
+<title>B.U.G. - Gruppi di Utenti di Brisk</title>
+<link rel="stylesheet" type="text/css" href="brisk.css">
+<link rel="stylesheet" type="text/css" href="room.css">
+<link rel="stylesheet" type="text/css" href="bug.css">
+<script type="text/javascript" src="commons.js"></script>
+</head>
+<body>
+<img class="nobo" style="position: static;" src="img/bug_bannerbig93.png">
+<div class="bug_cont">
+<br>
+<h3>Chi siamo.</h3>
+Un gruppo di frequentatori abituali del sito che ha deciso di sviluppare al suo interno un progetto di aggregazione tra utenti.
+<br>
+<br>
+<h3>Cosa ci interessa.</h3>
+Prima di tutto giocare a briscola chiamata (per chattare o conoscere gente nuova pensiamo ci siano mille altri modi).
+
+<br>
+<br>
+<h3>Quali sono i nostri obiettivi.</h3>
+Favorire il formarsi di gruppi locali di persone - i B.U.G. per l'appunto - che si trovino dal vivo, e non solo on-line, a giocare insieme per qualche ora.<br>
+Seguire lo sviluppo e proporre nuove funzionalità di Brisk, sia per il gioco, sia per gli strumenti utili agli scopi dei B.U.G.<br>
+Agevolare il supporto a Brisk mettendosi a disposizione come punto di raccolta per donazioni collettive.
+<br>
+<br>
+<h3>Come fondare un B.U.G.</h3>
+DA SCRIVERE ... BLA BLA 
+Sottoscrivere il manifesto 
+BLA BLA
+
+<br>
+<a name="pre_form">
+<br><h3>Ti interessa ?</h3>
+Se ti piacerebbe fondare un B.U.G. o conoscere quali sono quelli più vicini a te <a href="#pre_form" onClick="$('bug_form_out').style.display = 'block'; window.location.href = '#pre_form';" >clicca qui e compila il form</a>  per ricevere informazioni più dettagliate.<br><br>
+<div id="bug_form_out" class="bug_form_out">
+<div  id="bug_form" class="bug_form">
+<h3>&nbsp;&nbsp;Richiesta di maggiori informazioni:</h3>
+<form>
+<div class="bug_form_inn">
+<table class="bug_form">
+<tr><td>Nome<sup>*</sup>: </td><td><input name="name" type="text" size="40"></td></tr>
+<tr><td>Cognome<sup>*</sup>: </td><td><input name="surname" type="text" size="40"></td></tr>
+<tr><td>Nickname: </td><td><input name="nick" type="text" size="40"></td></tr>
+<tr><td>Età: </td><td><input name="age" type="text" size="4"></td></tr>
+<tr><td>Mezzi di trasporto: </td><td><input name="trasp" type="text" size="40"></td></tr>
+<tr><td>Nazione<sup>*</sup>: </td><td><input name="nat" type="text" size="40"></td></tr>
+<tr><td>Provincia<sup>*</sup>: </td><td>
+<select>
+<option>Seleziona
+<option>MI
+<option>BG
+</select>
+</td>
+</tr>
+<tr><td>Città<sup>*</sup>: </td><td><input name="city" type="text" size="40"></td></tr>
+<tr><td style="vertical-align: top;">Domanda<sup>*</sup>: </td><td>
+<TEXTAREA NAME="quest" ROWS="6" COLS="30">
+</TEXTAREA>
+</td></tr>
+<tr><td></td><td style="text-align: right;"><br><input class="button" type="submit" name="sub"  value="invia"></td></tr>
+</table>
+</div>
+</form>
+<sup>*</sup>: campi obbligatori.
+</div>
+</div>
+<br>
+<i><u>Gli amici di Brisk.</u></i>
+</div>
+</body>
+</html>
