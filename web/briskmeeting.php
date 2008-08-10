a3a94bbfaa5b0484a6d32bb04bde148f5f95347b
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2008-08-10 20:51:02 +0000

    update infos

--- web/briskmeeting.php
+++ web/briskmeeting.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" type="text/css" href="bug.css"> -->
 <script type="text/javascript" src="commons.js"></script>
 </head>
-<body style="font-family: verdana, sans-serif; font-size: 14px;">
+<body>
 <div style="margin: 40px auto auto auto; text-align: center;">
 <div style=" z-index: 0; margin: auto; background-color: #fafafa; background-repeat: no-repeat; background-image: URL('img/brisk_half.png'); background-position: center right; text-align: left; border: 1px solid black; position: relative; left: auto; width: 756px;">
 <img style="z-index: 50; position: absolute; border: 0px solid black; left: -26px; top: -26px; margin: 0px;" src="img/brisk_meeting_title_hi.png">
@@ -21,9 +21,56 @@ conoscerci meglio e soprattutto passare una giornata di divertimento
 giocando a briscola chiamata.<br>
 <h4>Date dei meeting</h4>
 <ul>
-<li>30 agosto - <a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=sarnico,+italy&sll=37.0625,-95.677068&sspn=52.902929,57.65625&ie=UTF8&z=13">Sarnico (BG)</a> sul lago d'Iseo</li>
-<li>7 settembre - vicinanze di <a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=parma,+italy&ie=UTF8&ll=44.80425,10.332642&spn=0.74832,0.900879&z=10&iwloc=addr" style="color:#0000FF;text-align:left">Parma (PR)</a></li>
+<li>30 agosto - <a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=12,+via+roma,+paratico,+brescia,+italy&sll=37.0625,-95.677068&sspn=35.547176,84.023438&ie=UTF8&ll=45.665376,9.953957&spn=0.015326,0.041027&z=15">Sarnico (Bergamo)</a> sul lago d'Iseo</li>
+<li>7 settembre - <a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=24,+strada+del+certosino,+ravadese,+parma,+italy&sll=44.858274,10.373986&sspn=0.069603,0.164108&ie=UTF8&ll=44.867419,10.393639&spn=0.069591,0.164108&z=13" style="color:#0000FF;text-align:left">Ravadese (Parma)</a></li>
 </ul>
+<br>
+<h4>Informazioni utili</h4>
+
+<table class="ricapito" >
+<tr><th class="thnobo"></th><th>Sarnico (Bergamo)</th><th>Ravedese (Parma)</th></tr>
+<tr><th>Quando:</th><td>30 Agosto</td>
+<td>7 Settembre</td></tr>
+<tr><th>Dove:</th><td><b>Hotel della stazione</b><br><a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=12,+via+roma,+paratico,+brescia,+italy&sll=37.0625,-95.677068&sspn=35.547176,84.023438&ie=UTF8&ll=45.665376,9.953957&spn=0.015326,0.041027&z=15">Via Roma, 12</a><br>Sarnico (Bergamo)</td>
+<td><b>Ca'Pianoni</b><br><a target="_blank" href="http://maps.google.com/maps?f=q&hl=it&geocode=&q=24,+strada+del+certosino,+ravadese,+parma,+italy&sll=44.858274,10.373986&sspn=0.069603,0.164108&ie=UTF8&ll=44.867419,10.393639&spn=0.069591,0.164108&z=13" style="color:#0000FF;text-align:left">Strada del Certosino,24</a><br>Ravedese (Parma)</td></tr>
+<tr><th>Costo<br>del pranzo:</th><td>23 €(<a href="#" onmouseover="$('bg_menu').style.display = '';" onmouseout="$('bg_menu').style.display = 'none';">menu</a>)
+<div id="bg_menu" style="text-align: left; position: absolute; display: none; background: white; border: 1px solid black; padding: 4px;">
+pranzo:<br>
+<ul>
+<li>2 primi a scelta
+<li>2 secondi a scelta
+<li>contorno
+<li>dolce
+<li>caffè
+</ul>
+</div>
+</td><td>25 € (<a href="#" onmouseover="$('pr_menu').style.display = '';" onmouseout="$('pr_menu').style.display = 'none';">menu</a>)
+<div id="pr_menu" style="text-align: left; position: absolute; display: none; background: white; border: 1px solid black; padding: 4px;">
+ verso le 11:00:<br>
+<ul>
+<li>aperitivo
+</ul>
+pranzo:<br>
+<ul>
+<li>antipasto di salumi
+<li>tortelli di erbette
+<li>assaggio di gnocchi di zucca
+<li>assaggio di risotto (o forse crepes) al radicchio rosso
+<li>tagliata
+<li>pollo fritto con verdure fritte
+<li>frutta
+<li>bis di torte
+<li>caffè
+<li>acqua e vino
+</ul>
+</div></td></tr>
+<tr><th>Chiusura<br>prenotazioni:</th><td>28/9</td><td>1/10</td></tr>
+<tr><th>Possibilità<br>di cenare:</th><td>Sì</td><td>No, salvo <br>richieste di massa<br>(min 10 persone)</td></tr>
+<tr><th>Possibilità<br> di pernottare:</th><td>Sì</td><td>No</td></tr>
+<tr><th title="per informazioni e prenotazioni contattateci!">Contatti e prenotazioni:</th><td><a href="mailto:grandebego@hotmail.it, susy.b@live.it?subject=briskmeeting_bg">Bego</a></td><td><a href="mailto:MANUEL-K2@hotmail.it?subject=briskmeeting_pr">Manuel-K2</a></td></tr>
+
+</table>
+
 <h4>Come si articoleranno le giornate</h4>
 <ul>
 <li>ritrovo in tarda mattinata (9:30-10:00)</li>
@@ -37,17 +84,6 @@ giocando a briscola chiamata.<br>
 <br>
 Il termine serale &egrave; lasciato naturalmente alle esigenze di ognuno.<br>
 
-<h4>Contatti</h4>
-Chi fosse interessato a partecipare a uno o entrambi questi eventi, oppure
-volesse delucidazioni, non esiti a contattare: <br>
-<dl>
-<dd><a href="mailto:grandebego@hotmail.it">Bego</a></dd>
-<dd><a href="mailto:MANUEL-K2@hotmail.it">Manuel-K2</a></dd>
-</dl>
-
-<h4>Aggiornamenti</h4>
-Fra pochi giorni renderemo noti, a chi vorr&agrave; partecipare, i dettagli  e le
-indicazioni stradali per raggiungere i siti dei 2 incontri.<br>
 <br>
 Saluti a tutti, Bego e Manuel.<br>
 <br>
