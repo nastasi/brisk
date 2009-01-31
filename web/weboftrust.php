1586185696acd6cf447ef14e8d08bffb7393001e
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2009-01-31 10:33:22 +0000

    page that explain how to get a password

---
+++ web/weboftrust.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Come ottenere user e password</title>
+</head>
+<body>
+<div style="text-align: center;">
+<br>
+<h2>Come ottenere user e password</h2>
+<br><br>
+Attualmente l' unico modo per ottenere uno user e una password<br> &egrave; avere un utente che gi&agrave; ne possiede una che garantisca per te,<br>li riconosci perch&eacute; hanno il nickname in <i>italico</i>,<br>chiedigli di usare il comando <code>/garante</code> e di inviare attraverso la maschera che gli apparir&agrave;<br>il nick name che preferisci e il tuo indirizzo di e-mail.
+<br><br>
+  Il team di brisk.
+  
+</div>
+</body>
+</html>
