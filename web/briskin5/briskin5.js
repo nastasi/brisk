9fe7c6c2413b91e1b738603d4fbc039e59d895db
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2009-02-21 17:41:40 +0000

    game related functions moved to briskin5

--- web/briskin5/briskin5.js
+++ web/briskin5/briskin5.js
@@ -27,3 +27,259 @@ function background_set()
 {
     $("bg").style.backgroundImage = 'url("img/brisk_table_sand'+table_pos+'.jpg")'; 
 }
+
+/* Stat: TABLE  Subst: ASTA */
+function act_asta(card,pnt)
+{
+    send_mesg("asta|"+card+"|"+pnt);
+}
+
+var asta_xarr = new Array(0,66,132);
+
+/* TODO: impostare gli onclick */
+function dispose_asta(idx, pnt, nopoint)
+{
+    var i, btn, pass;
+    var btn;
+    var lng = langtolng(g_lang);
+
+    for (i = 0 ; i < 10 ; i++) {
+	btn = $("asta"+i);
+	if (i < idx) {
+	    btn.src = "img/astapasso"+(pnt >= 0 ? "" : "_ro")+lng+".png";
+            btn.style.cursor = (pnt >= 0 ? "pointer" : "default");
+	    pass = -1;
+	}
+	else {
+	    btn.src = "img/asta"+i+(pnt >= 0 ? "" : "_ro")+lng+".png";
+            btn.style.cursor = (pnt >= 0 ? "pointer" : "default");
+	    pass = i;
+	}
+	if (i < 19)
+	    btn.style.left = asta_xarr[i % 3];
+	else
+	    btn.style.left = asta_xarr[(i+1) % 3];
+	
+	btn.style.top  = parseInt(i / 3) * 50 + (i == 9 ? 0 : 1);
+
+	if (pnt >= 0) {
+	    eval("btn.onclick = function () { act_asta("+pass+",61); }");
+	    btn.style.cursor = "pointer";
+	}
+	else {
+	    btn.onclick = null;
+	    btn.style.cursor = "default";
+	}
+    }
+    
+    
+    btn = $("astaptdiv");
+    btn.style.left = asta_xarr[i % 3];
+    btn.style.top = parseInt(i / 3) * 50 - 2;
+    // btn.style.visibility  = "visible";
+    
+    btn = $("astapt");
+    var rpnt = (pnt < 0 ? -pnt : pnt);
+    btn.value = (rpnt < 61 ? 61 : (rpnt > 120 ? 120 : rpnt));
+    
+    btn = $("astaptsub");
+    btn.style.left = asta_xarr[i % 3];
+    btn.style.top = 25 + parseInt(i / 3) * 50 - 1;
+    btn.src = "img/astaptsub"+(pnt >= 0 ? "" : "_ro")+lng+".png";
+    btn.style.cursor = (pnt >= 0 ? "pointer" : "default");
+    if (pnt >= 0) {
+	btn.onclick = function () { act_asta(9,$("astapt").value); };
+	btn.style.cursor = "pointer";
+    }
+    else {
+	btn.onclick = null;
+	btn.style.cursor = "default";
+    }
+    
+    i+=1;
+    if (nopoint) {
+	btn = $("astapasso");
+	btn.style.left = asta_xarr[i % 3];
+	btn.style.top = parseInt(i / 3) * 50;
+	btn.src = "img/astapashalf"+(pnt >= 0 ? "" : "_ro")+lng+".png";
+        btn.style.cursor = (pnt >= 0 ? "pointer" : "default");
+	if (pnt >= 0) {
+	    btn.onclick = function () { act_asta(-1,0); };
+	}
+	else {		
+	    btn.onclick = null;
+	}
+
+	btn = $("astalascio");
+	btn.style.left = asta_xarr[i % 3];
+	btn.style.top = parseInt(i / 3) * 50 + 24;
+	btn.src = "img/astalascio"+lng+".png";
+	btn.style.visibility = "visible";
+	btn.onclick = function () { safelascio(); };
+	}
+    else {
+	btn = $("astapasso");
+	btn.style.left = asta_xarr[i % 3];
+	btn.style.top = parseInt(i / 3) * 50;;
+	btn.src = "img/astapasso"+(pnt >= 0 ? "" : "_ro")+lng+".png";
+        btn.style.cursor = (pnt >= 0 ? "pointer" : "default");
+	if (pnt >= 0) {
+	    btn.onclick = function () { act_asta(-1,0); };
+	}
+	else {
+	    btn.onclick = null;
+	}
+
+	btn = $("astalascio");
+	btn.style.visibility = "hidden";
+	btn.onclick = null;
+    }
+    // btn.style.visibility  = "visible";
+    $("asta").style.visibility = "visible";
+}
+
+function asta_pnt_set(pnt)
+{
+    btn = $("astapt");
+    var rpnt = (pnt < 0 ? -pnt : pnt);
+    btn.value = (rpnt < 61 ? 61 : (rpnt > 120 ? 120 : rpnt));
+}
+
+function hide_asta()
+{
+    $("asta").style.visibility = "hidden"; 
+}
+
+function choose_seed(card)
+{
+    var i;
+
+    $("asta").style.visibility = "hidden"; 
+    $("astalascio").style.visibility = "hidden"; 
+    $("chooseed").style.visibility = "visible";
+    for (i = 0 ; i < 4 ; i++) {
+	$("seed"+i).src = "img/"+i+""+card+".png";
+	seed=$("seed"+i);
+	eval("seed.onclick = function () { act_choose("+i+""+card+"); };");
+    }
+}
+
+var astat_suffix = new Array("","_ea","_ne","_nw","_we");
+
+function show_astat(zer,uno,due,tre,qua)
+{
+    var astat = new Array(zer,uno,due,tre,qua);
+    var lng = langtolng(g_lang);
+
+    for (i = 0 ; i < PLAYERS_N ; i++) {
+	idx = (PLAYERS_N + i - table_pos) % PLAYERS_N;
+
+	if (astat[i] == -2) {
+	    $("public"+astat_suffix[idx]).style.visibility = "hidden";
+	}
+	else if (astat[i] == -1) {
+	    $("public"+astat_suffix[idx]).style.visibility = "visible";
+	    $("pubacard"+astat_suffix[idx]).src = "img/astapasso"+lng+".png";
+	    $("pubapnt"+astat_suffix[idx]).innerHTML = "";
+	    $("pubapnt"+astat_suffix[idx]).style.visibility = "hidden";
+	}
+	else if (astat[i] <= 10) {
+	    $("public"+astat_suffix[idx]).style.visibility = "visible";
+	    $("pubacard"+astat_suffix[idx]).src = "img/asta"+astat[i]+lng+".png";
+	    $("pubapnt"+astat_suffix[idx]).style.visibility = "hidden";
+	}
+	else if (astat[i] <= 120) {
+	    $("public"+astat_suffix[idx]).style.visibility = "visible";
+	    $("pubacard"+astat_suffix[idx]).src = "img/asta9"+lng+".png";
+	    $("pubapnt"+astat_suffix[idx]).style.visibility = "inherit"; // XXX VISIBLE
+	    $("pubapnt"+astat_suffix[idx]).innerHTML = astat[i];
+	}
+    }
+}
+
+
+function table_init() {
+    var sux = new Array("", "_ea", "_ne", "_nw", "_we");
+
+    // console.log("table_init");
+
+    remark_off();
+    $("asta").style.visibility = "hidden";
+    $("caller").style.visibility = "hidden";
+    show_astat(-2,-2,-2,-2,-2);
+    for (i=0 ; i < 8 ; i++) {
+	Drag.init($("card" + i), card_mouseup_cb);
+	for (e = 0 ; e < PLAYERS_N ; e++)
+	    $("card"+sux[e]+i).style.visibility = "hidden";
+    }
+    for (i=0 ; i < PLAYERS_N ; i++) {
+        // console.log("shut: "+"takes"+sux[i]);
+	$("takes"+sux[i]).style.visibility = "hidden";
+	}
+
+    for (i = 0 ; i < 8 ; i++) {
+	cards_pos[i] = i;
+	cards_ea_pos[i] = i;
+	cards_ne_pos[i] = i;
+	cards_nw_pos[i] = i;
+	cards_we_pos[i] = i;
+    }
+
+}
+  
+function act_choose(card)
+{
+    send_mesg("choose|"+card);
+}
+
+function act_play(card,x,y)
+{
+    send_mesg("play|"+card+"|"+x+"|"+y);
+}
+
+function act_tableinfo()
+{
+    send_mesg("tableinfo");
+}
+
+function act_exitlock()
+{
+    send_mesg("exitlock");
+}
+
+function safelogout()
+{
+    var res;
+    
+    if (g_exitlock < 2) 
+	res = window.confirm("Sei sicuro di volere abbandonare la partita?\nATTENZIONE: se esci adesso senza il consenso degli altri giocatori non potrai sederti ai tavoli per "+(Math.floor(EXIT_BAN_TIME/60))+" minuti.");    
+    else 
+	res = window.confirm("Sei sicuro di volere abbandonare la partita?");
+    if (res)
+	act_logout(g_exitlock);
+}
+
+function act_reload()
+{
+    window.onunload = null;
+    window.onbeforeunload = null;
+    document.location.reload();
+}
+
+function set_names(so,ea,ne,nw,we)
+{
+//    alert("EA: "+ea);
+    $("name").innerHTML = italizer(so);
+    $("name").title = unescapeHTML(so[1]); 
+    $("name_ea").innerHTML = italizer(ea);
+    $("name_ea").title = unescapeHTML(ea[1]);
+    $("name_ne").innerHTML = italizer(ne);
+    $("name_ne").title = unescapeHTML(ne[1]);
+    $("name_nw").innerHTML = italizer(nw);
+    $("name_nw").title = unescapeHTML(nw[1]);
+    $("name_we").innerHTML = italizer(we);
+    $("name_we").title = unescapeHTML(we[1]);
+
+    return;
+}
+
