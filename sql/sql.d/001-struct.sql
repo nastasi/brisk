740196fc9900bb163eed6797118e14885ba73f08
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2011-02-02 08:23:35 +0100

    UNIQUE constraint added to login and email fields

--- sql/sql.d/001-struct.sql
+++ sql/sql.d/001-struct.sql
@@ -4,15 +4,15 @@
 
 DROP TABLE #PFX#users;
 CREATE TABLE #PFX#users (
-                        code  integer PRIMARY KEY,
-                        login text,
+                        code  SERIAL PRIMARY KEY,
+                        login text UNIQUE,
                         pass  text,
-                        email text,
+                        email text UNIQUE,
                         type  integer
                         );
 
 DROP TABLE #PFX#groups;
 CREATE TABLE #PFX#groups (
-                        code  integer PRIMARY KEY,
+                        code  SERIAL PRIMARY KEY,
                         gname text
                         );
