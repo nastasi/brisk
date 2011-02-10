9fba6a242795ed609150c23ab6ea886a3b78e718
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2011-02-10 18:56:01 +0100

    tsusp and mtime field added to users table (disable timeout and last access), rename field gname in name in groups table

--- sql/sql.d/001-struct.sql
+++ sql/sql.d/001-struct.sql
@@ -4,15 +4,19 @@
 
 DROP TABLE #PFX#users;
 CREATE TABLE #PFX#users (
-                        code  SERIAL PRIMARY KEY,
-                        login text UNIQUE,
-                        pass  text,
-                        email text UNIQUE,
-                        type  integer
-                        );
+       code   SERIAL PRIMARY KEY,
+       login  text UNIQUE,
+       pass   text,
+       email  text UNIQUE,
+       type   integer,
+       tsusp  timestamp DEFAULT to_timestamp(0) -- disable timeout
+       mtime  timestamp DEFAULT to_timestamp(0) -- last access
+       );
 
 DROP TABLE #PFX#groups;
 CREATE TABLE #PFX#groups (
-                        code  SERIAL PRIMARY KEY,
-                        gname text
-                        );
+       code   SERIAL PRIMARY KEY,
+       name   text
+       );
+
+
