2054502215862619b019d6ffccb6692a9b5281ed
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2011-02-13 17:26:06 +0100

    data for places and to archive match points

---
+++ sql/sql.d/050-bin5.sql
@@ -0,0 +1,25 @@
+--
+-- briskin5 (bin5) related tables
+--
+
+DROP TABLE #PFX#bin5_matches;
+CREATE TABLE #PFX#bin5_matches (
+       code   SERIAL PRIMARY KEY,
+       ttok   text UNIQUE,              -- token associated to the match
+       tidx   integer                   -- table index
+       );
+
+DROP TABLE #PFX#bin5_games;
+CREATE TABLE #PFX#bin5_games (
+       code   SERIAL PRIMARY KEY,
+       mcode  integer REFERENCES #PFX#bin5_matches (code) ON DELETE cascade ON UPDATE cascade,
+       tstamp timestamp                 -- end game time
+       );
+       
+DROP TABLE #PFX#bin5_points;
+CREATE TABLE #PFX#bin5_points (
+       gcode  integer REFERENCES #PFX#bin5_games (code) ON DELETE cascade ON UPDATE cascade,
+       ucode  integer REFERENCES #PFX#users (code) ON DELETE cascade ON UPDATE cascade, 
+       pts    integer                   -- points                 
+       );
+
---
+++ sql/sql.d/051-bin5-places.sql
@@ -0,0 +1,25 @@
+--
+-- try to start with a single table for all placings
+--
+-- tri = 0     lo = 0
+-- mon = 2     hi = 1
+-- wee = 4
+--
+
+DROP   TABLE #PFX#bin5_places_mtime;
+CREATE TABLE #PFX#bin5_places_mtime (
+       code   int,
+       mtime  timestamp
+);
+INSERT INTO #PFX#bin5_places_mtime (code, mtime) VALUES (0, now());
+
+DROP   TABLE #PFX#bin5_places;
+CREATE TABLE #PFX#bin5_places (
+       type   integer,
+       rank   integer,
+       ucode  integer REFERENCES #PFX#users (code) ON DELETE cascade ON UPDATE cascade, 
+       login  text,
+       pts    integer,
+       games  integer,
+       score  float
+);
