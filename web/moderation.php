a79944437726160aa789972b1cd42838c67f4148
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2013-05-28 07:22:40 +0200

    moderation: enable and disable window

---
+++ web/moderation.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Moderation</title>
+<script type="text/javascript"><!--
+window.is_loaded = false;
+window.onload = function() {
+    window.is_loaded = true;     
+}
+// -->
+</script>
+</head>
+<body>
+<?php
+  // sleep(10);
+?>
+<div id="mainbody"/>
+</body>
+</html>
