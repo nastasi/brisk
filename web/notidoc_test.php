76e60a0d70700e42468b35921854fecc929254c5
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2013-11-18 07:38:55 +0100

    new notifier method and associated test

---
+++ web/notidoc_test.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title><?php echo "$PHP_SELF";?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script type="text/javascript" src="commons.js"></script>
+
+<!-- <script type="text/javascript" src="myconsole.js"></script> -->
+
+<script type="text/javascript"><!--
+     var sess = "for_test";
+     var stat = "";
+     var subst = "";
+     var gst = new globst();
+     var nd;
+
+     window.onload = function() {
+         var i, sss = "";
+
+         for (i = 0 ; i < 20 ; i++) {
+             sss += "solo una prova<br>";
+         }
+
+         nd = new notify_document(gst, sss, 4000, "pippo", 200, 200, true, 2000);
+     }
+ //-->
+</script>
+<link rel="stylesheet" type="text/css" href="brisk.css">
+</head>
+         <!--  style="position: static; width: 800px; height: 600px; background-color: yellow;" -->
+<body>
+         <!--  style="position: static; width: 600px; height: 400px; background-color: pink;" -->
+<div>
+div in body
+</div>
+</body>
+</html>
