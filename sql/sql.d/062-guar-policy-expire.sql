f4178d38662a52ae2ccc8957e499621e735f4d16
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2013-11-27 18:38:05 +0100

    disa_reas field added to users table

--- sql/sql.d/062-guar-policy-expire.sql
+++ sql/sql.d/062-guar-policy-expire.sql
@@ -1,3 +1,8 @@
 -- add current licence version accepted by users
 ALTER TABLE #PFX#users DROP COLUMN lice_vers;
 ALTER TABLE #PFX#users ADD COLUMN lice_vers text DEFAULT '';  -- current accepted licence version
+
+-- add reason field for disabled users
+ALTER TABLE #PFX#users DROP COLUMN disa_reas;
+ALTER TABLE #PFX#users ADD COLUMN disa_reas integer DEFAULT 0;  -- current disable reason
+
