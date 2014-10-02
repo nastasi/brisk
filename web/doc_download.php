7e4dfd67f0e1a1293c6cbbedd88c9100b0204c92
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2014-10-02 17:53:25 +0200

    tos can be download as pdf or txt now

--- web/doc_download.php
+++ web/doc_download.php
@@ -9,12 +9,19 @@ function main()
         return(FALSE);
     }
 
-    $fname = sprintf("%s%s.pdf", $G_doc_path, basename($_GET['doc']));
+    $ext = "pdf";
+    $cont_type = "application/octet-stream";
+    if (isset($_GET['ext']) && ($_GET['ext'] == "txt")) {
+        $cont_type = "plain/text";
+        $ext = $_GET['ext'];
+    }
+
+    $fname = sprintf("%s%s.%s", $G_doc_path, basename($_GET['doc']), $ext);
     if (! file_exists($fname)) {
         return(FALSE);
     }
 
-    header("Content-Type: application/octet-stream");
+    header(sprintf("Content-Type: %s", $cont_type));
     header(sprintf("Content-Disposition: attachment; filename=brisk_%s", basename($fname)));
     readfile($fname);
     return(TRUE);
