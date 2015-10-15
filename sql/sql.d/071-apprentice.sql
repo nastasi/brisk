a04917816df17fb43979ecb4299d84ac3ae7e6e7
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2015-10-15 18:38:30 +0200

    add game_cnt and match_cnt fields to bsk_users table

--- sql/sql.d/071-apprentice.sql
+++ sql/sql.d/071-apprentice.sql
@@ -6,3 +6,11 @@ CREATE TABLE #PFX#selfreg_chk (
        ip         integer,                           -- ip v4 address
        atime      timestamp DEFAULT to_timestamp(0)  -- access time
        );
+
+--
+--  Add counters to show how many matches and games are played
+--
+ALTER TABLE #PFX#users DROP COLUMN match_cnt;
+ALTER TABLE #PFX#users ADD COLUMN match_cnt integer DEFAULT 0;
+ALTER TABLE #PFX#users DROP COLUMN game_cnt;
+ALTER TABLE #PFX#users ADD COLUMN game_cnt integer DEFAULT 0;
