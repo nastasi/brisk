72c580d5abfe6772618361720eeffdfeacbf2458
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2017-03-18 11:01:12 +0100

    use config file to run explain_update

--- bin/brisk_explain_update.sh
+++ bin/brisk_explain_update.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-set -x
+# set -x
 export PATH=/home/nastasi/bin:/usr/local/bin:/usr/bin:/bin:/usr/games
-B_HOSTNAME="http://localhost/brisk"
+
+CONFIG_FILE="$HOME/.brisk_install"
+
+# default values
+web_url="http://localhost/brisk"
+admin_password=""
+if [ -f "$CONFIG_FILE" ]; then
+   source "$CONFIG_FILE"
+   conffile_in="$CONFIG_FILE"
+fi
+
+tty -s
+is_a_tty=$?
+if [ -z "$admin_password" ]; then
+    if [ "$is_a_tty" -eq 0 ]; then
+        read -s -p "Please insert admin password (no echo): " admin_password
+    else
+        echo "Incomplete configuration"
+        exit 1
+    fi
+fi
+
 # now="$(date -d '2014-01-21 23:56:00' +%s)"
 now="$(date +%s)"
 
@@ -10,4 +31,4 @@ from="$(date +"%Y-%m-%d+%H:%M:%S" -d @$(echo "($now / 86400) * 86400 + 7200 - (8
 # to="$(date +"%Y-%m-%d+%H:%M:%S" -d @$(echo "$now + 7200 " | bc))"
 # from="$(date +"%Y-%m-%d+%H:%M:%S" -d @$(echo "$now - 9200 " | bc))"
 
-curl -d 'pazz=yourpasswd' "$B_HOSTNAME/briskin5/stat-day.php?from=$from&to=$to"
+curl -d "pazz=$admin_password" "$web_url/briskin5/stat-day.php?from=$from&to=$to"
