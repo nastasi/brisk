d64e0e2c896d74ba403bc3e924ceafc568d8c82b
Matteo Nastasi (mop) <nastasi@alternativeoutput.it>
2017-03-19 10:48:11 +0100

    more consistent matches descriptions

--- bin/brisk_explain_update.sh
+++ bin/brisk_explain_update.sh
@@ -25,7 +25,7 @@ fi
 
 # now="$(date -d '2014-01-21 23:56:00' +%s)"
 now="$(date +%s)"
-delta="0"
+delta="86400"
 
 to="$(date +"%Y-%m-%d+%H:%M:%S" -d @$(echo "($now / 86400) * 86400 + 7200 + $delta" | bc))"
 from="$(date +"%Y-%m-%d+%H:%M:%S" -d @$(echo "($now / 86400) * 86400 + 7200 - (86400) + $delta" | bc))"
