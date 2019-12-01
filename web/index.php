47ae15c35f3c407208e52329949039a55e1f3693
Matteo Nastasi <nastasi@alternativeoutput.it>
2019-12-01 11:49:53 +0100

    update homepage to manage adsense advertisement wrong rendering

--- web/index.php
+++ web/index.php
@@ -682,19 +682,19 @@ google_color_url = "000000";
     $brisk_header_form = '<div class="container">
 <!-- =========== header ===========  -->
 <div id="header" class="header">
-<table width="100%%" border="0" cols="3"><tr>
-<td align="left"><div style="padding-left: 8px;">'.$banner_top_left.'</div></td>
-<td align="center">'.(($G_with_topbanner || $G_with_donors) ? '<table><tr><td>' : '').'<div style="text-align: center;">
+<table width="100%%" style="min-height: 84px;" border="0" cols="3"><tr>
+<td style="width: 33%%;" align="left"><div style="padding-left: 8px;">'.$banner_top_left.'</div></td>
+<td style="width: 34%%;" align="center">'.(($G_with_topbanner || $G_with_donors) ? '<table><tr><td>' : '').'<div style="text-align: center;">
  <img class="nobo" src="img/brisk_logo64.png">'
 /*    <img class="nobo" src="img/brisk_logo64_blackribbon.png" title="ciao Prof" alt="ciao Prof">' */
     .$mlang_room['headline'][$G_lang].'<br>
     </div>'.( ($G_with_topbanner || $G_with_donors) ? sprintf('</td><td>%s</td></tr></table>',
                                                                 ($G_with_topbanner ? $G_topbanner :
 "<div style='background-color: #ffd780; border: 1px solid black; text-align: center;'><img class='nobo' src=\"donometer.php?c=".$G_donors_cur."&a=".$G_donors_all."\"><div style='padding: 1px; background-color: white;'><b>donatori</b></div></div>") ) : '').'</td>
-<td align="right"><div style="padding-right: 8px;">
-'.$banner_top_right.'</div></td>
+<td style="width: 33%%;" align="right"><div style="padding-right: 8px;">'.$banner_top_right.'</div></td>
 </tr></table>
 </div>';
+// <td style="width: 33%%;" align="right">'.$banner_top_right.'</td>
 
     /* MLANG: ALL THE VERTICAL MENU */
     $brisk_vertical_menu = '
